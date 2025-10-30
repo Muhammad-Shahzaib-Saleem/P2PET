7965c340f8222f9c613d3315505e9ac87a613753
mAc BoOk pRo <dotenterprises@mAcs-MacBook-Pro.local>
2025-10-30 20:22:58 +0500

    deploying frontend

--- P2PET/energy-trade-frontend/src/api/api.js
+++ P2PET/energy-trade-frontend/src/api/api.js
@@ -4,7 +4,8 @@ import axios from "axios";
 // Auto-detect host (works when you open UI from another device on LAN)
 const host = window.location.hostname;
 // Prefer .env override: VITE_API_BASE=http://<pi-ip>:8000
-export const API_BASE = "http://localhost:8000/";
+export const API_BASE =
+  "https://substances-thrown-crystal-thirty.trycloudflare.com/";
 
 // Shared axios instance (you can add interceptors/logging here later)
 const api = axios.create({
