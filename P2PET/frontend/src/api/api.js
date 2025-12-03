e2724edef328936b55c33fa9efa2e0e85ff775a6
mAc BoOk pRo <dotenterprises@mAcs-MacBook-Pro.local>
2025-12-03 13:24:50 +0500

    refactoring

--- P2PET/frontend/src/api/api.js
+++ P2PET/frontend/src/api/api.js
@@ -4,8 +4,7 @@ import axios from "axios";
 // Auto-detect host (works when you open UI from another device on LAN)
 const host = window.location.hostname;
 // Prefer .env override: VITE_API_BASE=http://<pi-ip>:8000
-export const API_BASE =
-  "https://essex-zum-prerequisite-diego.trycloudflare.com/";
+export const API_BASE = "http://192.168.0.145:8000/";
 
 // Shared axios instance (you can add interceptors/logging here later)
 const api = axios.create({
